5ffe1c1ab454bcf33938b7b955c6d4b23bd954d4
Pablo Pardo <pablopardoalcocer@gmail.com>
2024-05-11 00:08:19 -0400

    Update Banner component styles for improved responsiveness

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,11 +2,11 @@ import BannerImg from "/exam.jpg";
 
 export const Banner = () => {
   return (
-    <section className="relative font-bold h-[calc(100vh-150px)] w-screen min-h-[600px] bg-brand-darkblue flex items-center justify-center">
+    <section className="relative font-bold h-[60vh] min-h-[360px] bg-brand-darkblue flex items-center justify-center">
       <img
         src={BannerImg}
         alt="Examen de ingreso"
-        className="opacity-50 w-full h-full object-cover absolute top-0 left-0 z-0 filter blur-[10px] brightness-50 contrast-150 grayscale-100 invert-0 saturate-100 sepia-0"
+        className="opacity-45 w-full h-full object-cover absolute top-0 left-0 z-0 filter blur-[0.3rem] brightness-[.66] contrast-150 grayscale-100 invert-0 saturate-100 sepia-0"
       />
       <div className="w-full h-full p-3 flex flex-col gap-6 items-center justify-center text-center md:text-xl max-w-[600px] z-10">
         <p>
