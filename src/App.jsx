fc29d9709dcda50cb7cf53b80d70e1018580ab75
Pablo Pardo <pablopardoalcocer@gmail.com>
2024-05-11 22:07:31 -0400

    Add Footer component and social media icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Banner } from "./components/Banner";
 import { Filter } from "./components/Filter";
+import { Footer } from "./components/Footer";
 
 export const App = () => {
   return (
     <>
       <Banner />
       <Filter />
+      <Footer />
     </>
   );
 };
