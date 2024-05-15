82ccab2e85b10e51e2b5ae250422dc370df7123f
Pablo Pardo <pablopardoalcocer@gmail.com>
2024-05-15 11:38:22 -0400

    Add ErrorCountdown and Loader components for error handling and loading feedback

---
+++ src/assets/ErrorCountdown.jsx
@@ -0,0 +1,36 @@
+import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+
+export const ErrorCountdown = ({ seconds }) => {
+  const [count, setCount] = useState(seconds);
+
+  useEffect(() => {
+    if (count > 0) {
+      const timerId = setTimeout(() => {
+        setCount(count - 1);
+      }, 1000);
+      return () => clearTimeout(timerId);
+    }
+  }, [count]);
+
+  return (
+    <div>
+      {count > 0 ? (
+        <article className="m-6 max-w-96 flex flex-col">
+          <h3 className="text-3xl font-bold">Oops!</h3>
+          <span className="text-2xl font-semibold">Hubo un error :(</span>
+          <span className="text-xl">
+            El Examen de Ingreso-inador se reiniciará en {count} segundos...
+          </span>
+        </article>
+      ) : (
+        <Navigate to="/" />
+      )}
+    </div>
+  );
+};
+
+ErrorCountdown.propTypes = {
+  seconds: PropTypes.number.isRequired,
+};
--- src/components/PdfPage.jsx
+++ src/components/PdfPage.jsx
@@ -1,3 +1,4 @@
+import { Loader } from "../assets/Loader";
 import { Viewer } from "@react-pdf-viewer/core";
 import { zoomPlugin } from "@react-pdf-viewer/zoom";
 import { getFilePlugin } from "@react-pdf-viewer/get-file";
@@ -5,6 +6,7 @@ import { useParams } from "react-router-dom";
 import PropTypes from "prop-types";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/zoom/lib/styles/index.css";
+import { ErrorCountdown } from "../assets/ErrorCountdown";
 
 export const urlExamTemplate =
   "http://sagaa.fcyt.umss.edu.bo/adm_academica/archivos/examenes/{YEAR}-{SEMESTER}-{ID}/1/6-{FORM_VERSION}.pdf";
@@ -48,6 +50,10 @@ export const PdfPage = ({ pdfContentLabel }) => {
           )}
           theme="dark"
           plugins={[zoomPluginInstance, getFilePluginInstance]}
+          renderLoader={(percentages) => (
+            <Loader percentages={percentages} size={100} fill="#f3f4f6" />
+          )}
+          renderError={() => <ErrorCountdown seconds={3} />}
         />
       </div>
     </div>
