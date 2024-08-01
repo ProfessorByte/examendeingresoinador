fc0cfb7fa08aa9884d3765c255e56eec905cc032
Pablo Pardo <pablopardoalcocer@gmail.com>
2024-07-31 23:21:42 -0400

    Update Banner component to use new logo image source

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,25 @@
-import BannerImg from "/logo.webp";
+import { useEffect } from "react";
+import LogoImg from "/logo.webp";
 
 export const Banner = () => {
+  useEffect(() => {
+    const linkPreload = document.createElement("link");
+    linkPreload.rel = "preload";
+    linkPreload.as = "image";
+    linkPreload.href = LogoImg;
+    linkPreload.type = "image/webp";
+    document.head.appendChild(linkPreload);
+
+    return () => {
+      document.head.removeChild(linkPreload);
+    };
+  }, []);
+
   return (
     <header className="p-9 flex justify-center items-center">
       <img
-        src={BannerImg}
-        alt="Examen de ingreso"
+        src={LogoImg}
+        alt="Logo con el título Examen de Ingreso-inador"
         className="w-96 rounded-3xl"
         // eslint-disable-next-line react/no-unknown-property
         fetchpriority="high"
