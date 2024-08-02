5712adbd0ce81661e5e61b5c5cc8fbe391c83f51
Pablo Pardo <pablopardoalcocer@gmail.com>
2024-08-02 01:20:36 -0400

    Update image sources and optimize code structure

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,28 +1,10 @@
-import { useEffect } from "react";
-import LogoImg from "/logo.webp";
-
 export const Banner = () => {
-  useEffect(() => {
-    const linkPreload = document.createElement("link");
-    linkPreload.rel = "preload";
-    linkPreload.as = "image";
-    linkPreload.href = LogoImg;
-    linkPreload.type = "image/webp";
-    document.head.appendChild(linkPreload);
-
-    return () => {
-      document.head.removeChild(linkPreload);
-    };
-  }, []);
-
   return (
     <header className="p-9 flex justify-center items-center">
       <img
-        src={LogoImg}
+        src="/logo.webp"
         alt="Logo con el título Examen de Ingreso-inador"
-        className="w-96 rounded-3xl"
-        // eslint-disable-next-line react/no-unknown-property
-        fetchpriority="high"
+        className="w-96 rounded-3xl aspect-[27/20"
       />
     </header>
   );
