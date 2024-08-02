329542a627858e8602306c1b9d064d1b9b62ab6f
Pablo Pardo <pablopardoalcocer@gmail.com>
2024-08-02 01:20:36 -0400

    Update image sources and optimize code structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,10 @@ import { Footer } from "../components/Footer";
 
 export const Home = () => {
   return (
-    <div className="min-h-dvh flex flex-col justify-between">
-      <section>
-        <Banner />
-        <Filter />
-      </section>
+    <>
+      <Banner />
+      <Filter />
       <Footer />
-    </div>
+    </>
   );
 };
