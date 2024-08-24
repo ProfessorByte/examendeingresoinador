950c7578a3301e9cbe6c27fdcb35be37e58b3907
Pablo Pardo <37822461+ProfessorByte@users.noreply.github.com>
2024-08-23 21:08:42 -0400

    Update Banner component to include hidden h1 element to enhance SEO

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,12 @@
 export const Banner = () => {
   return (
-    <header className="p-9 flex justify-center items-center">
+    <header className="p-9 flex flex-col justify-center items-center">
       <img
         src="/logo.webp"
         alt="Logo con el título Examen de Ingreso-inador"
         className="w-96 rounded-3xl animate-fadeDown"
       />
+      <h1 className="hidden">Examen de Ingreso-inador</h1>
     </header>
   );
 };
