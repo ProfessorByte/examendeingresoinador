6580a5a585c0f98293d60d79cbd7a1e4537bb156
Pablo Pardo <pablopardoalcocer@gmail.com>
2024-12-12 23:38:07 -0400

    Add new PDF response files for various FCyT exams and remove unused PDF fetching utility

--- src/app/exams/fcyt/[slug]/page.tsx
+++ src/app/exams/fcyt/[slug]/page.tsx
@@ -1,33 +1,78 @@
-import { ExamSolutionSplit } from "@/components/ExamSolutionSplit";
-import { getPdfBlob } from "@/utils/services";
+"use client";
 
-const examsUrlTemplate =
-  "https://raw.githubusercontent.com/ProfessorByte/science-exams-crawler/refs/heads/main/downloads/{SLUG}/{CONTENT_LABEL}_{SLUG}.pdf";
+import { Gutter } from "@/components/Gutter";
+import { PdfPage } from "@/components/PdfPage";
+import { Direction } from "@/utils/interfaces";
+import { Worker } from "@react-pdf-viewer/core";
+import { useParams } from "next/navigation";
+import { SetStateAction, useEffect, useState } from "react";
+import Split from "react-split-grid";
 
-interface ExamsPageProps {
-  params: Promise<{ slug: string }>;
-}
+export default function ExamsPage() {
+  const [gridTemplate, setGridTemplate] = useState<string>("1fr 18px 1fr");
+  const [directionSplit, setDirectionSplit] = useState<Direction>(
+    window.innerWidth < 768 ? "row" : "column"
+  );
 
-export default async function ExamsPage({ params }: ExamsPageProps) {
-  const { slug } = await params;
+  const { slug } = useParams<{ slug: string }>();
 
-  const [examPdfBlob, solutionPdfBlob] = await Promise.all([
-    getPdfBlob(
-      examsUrlTemplate
-        .replace(/{SLUG}/g, slug)
-        .replace("{CONTENT_LABEL}", "Preguntas")
-    ),
-    getPdfBlob(
-      examsUrlTemplate
-        .replace(/{SLUG}/g, slug)
-        .replace("{CONTENT_LABEL}", "Respuestas")
-    ),
-  ]);
+  useEffect(() => {
+    const handleResize = () => {
+      setDirectionSplit(window.innerWidth < 768 ? "row" : "column");
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const setSplitProps = (direction: Direction) => {
+    if (direction === "row") {
+      return {
+        gridTemplateColumns: "1fr",
+        columnMinSize: undefined,
+        gridTemplateRows: gridTemplate,
+        cursor: "row-resize",
+        rowMinSize: 36,
+      };
+    }
+    return {
+      gridTemplateRows: "1fr",
+      rowMinSize: undefined,
+      gridTemplateColumns: gridTemplate,
+      cursor: "col-resize",
+      columnMinSize: 36,
+    };
+  };
+
+  const handleDrag = (
+    direction: Direction,
+    track: number,
+    style: SetStateAction<string>
+  ) => {
+    setGridTemplate(style);
+  };
 
   return (
-    <ExamSolutionSplit
-      examPdfBlob={examPdfBlob}
-      solutionPdfBlob={solutionPdfBlob}
-    />
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+      <Split
+        {...setSplitProps(directionSplit)}
+        onDrag={handleDrag}
+        // @ts-expect-error: Split component does not have TypeScript definitions for render prop
+        render={({ getGridProps, getGutterProps }) => (
+          <div className="grid h-dvh" {...getGridProps()}>
+            <PdfPage pdfContentLabel="exam" slug={slug} />
+            <Gutter
+              direction={directionSplit}
+              getGutterProps={getGutterProps}
+              track={1}
+            />
+            <PdfPage pdfContentLabel="solution" slug={slug} />
+          </div>
+        )}
+      />
+    </Worker>
   );
 }
