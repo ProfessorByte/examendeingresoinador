ebecee54a6782be7d9a96cee234aec58be97b5d0
Pablo Pardo <pablopardoalcocer@gmail.com>
2024-12-14 22:52:50 -0400

    Rename Icons.jsx to Icons.tsx and add TypeScript typings for icon components

--- src/assets/Icons.tsx
+++ src/assets/Icons.tsx
@@ -1,4 +1,8 @@
-export const InstagramIcon = (props) => (
+import { JSX, SVGProps } from "react";
+
+interface IconProps extends JSX.IntrinsicAttributes, SVGProps<SVGSVGElement> {}
+
+export const InstagramIcon = (props: IconProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={100}
@@ -11,7 +15,7 @@ export const InstagramIcon = (props) => (
   </svg>
 );
 
-export const FacebookIcon = (props) => (
+export const FacebookIcon = (props: IconProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={100}
@@ -24,7 +28,7 @@ export const FacebookIcon = (props) => (
   </svg>
 );
 
-export const XIcon = (props) => (
+export const XIcon = (props: IconProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={100}
@@ -37,7 +41,7 @@ export const XIcon = (props) => (
   </svg>
 );
 
-export const LinkedInIcon = (props) => (
+export const LinkedInIcon = (props: IconProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={100}
@@ -50,7 +54,7 @@ export const LinkedInIcon = (props) => (
   </svg>
 );
 
-export const GitHubIcon = (props) => (
+export const GitHubIcon = (props: IconProps) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" {...props}>
     <title>GitHub</title>
     <path d="M17.791 46.836A1.999 1.999 0 0 0 19 45v-5.4c0-.197.016-.402.041-.61A.611.611 0 0 1 19 39h-3.6c-1.5 0-2.8-.6-3.4-1.8-.7-1.3-1-3.5-2.8-4.7-.3-.2-.1-.5.5-.5.6.1 1.9.9 2.7 2 .9 1.1 1.8 2 3.4 2 2.487 0 3.82-.125 4.622-.555C21.356 34.056 22.649 33 24 33v-.025c-5.668-.182-9.289-2.066-10.975-4.975-3.665.042-6.856.405-8.677.707a21.537 21.537 0 0 1-.151-.987c1.797-.296 4.843-.647 8.345-.714a8.162 8.162 0 0 1-.291-.849c-3.511-.178-6.541-.039-8.187.097-.02-.332-.047-.663-.051-.999 1.649-.135 4.597-.27 8.018-.111a9.832 9.832 0 0 1-.13-1.543c0-1.7.6-3.5 1.7-5-.5-1.7-1.2-5.3.2-6.6 2.7 0 4.6 1.3 5.5 2.1C21 13.4 22.9 13 25 13s4 .4 5.6 1.1c.9-.8 2.8-2.1 5.5-2.1 1.5 1.4.7 5 .2 6.6 1.1 1.5 1.7 3.2 1.6 5 0 .484-.045.951-.11 1.409 3.499-.172 6.527-.034 8.204.102-.002.337-.033.666-.051.999-1.671-.138-4.775-.28-8.359-.089a8.272 8.272 0 0 1-.325.98c3.546.046 6.665.389 8.548.689-.043.332-.093.661-.151.987-1.912-.306-5.171-.664-8.879-.682-1.665 2.878-5.22 4.755-10.777 4.974V33c2.6 0 5 3.9 5 6.6V45c0 .823.498 1.53 1.209 1.836C41.37 43.804 48 35.164 48 25 48 12.318 37.683 2 25 2S2 12.318 2 25c0 10.164 6.63 18.804 15.791 21.836z" />
