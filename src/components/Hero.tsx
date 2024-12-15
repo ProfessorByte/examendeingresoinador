846e22670bc60cdfae6f39a3c95e958cb058c30c
Pablo Pardo <pablopardoalcocer@gmail.com>
2024-12-14 21:45:36 -0400

    Update alt text for images in Footer and Hero components for improved accessibility

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ export const Hero = () => {
     <header className="p-9 flex flex-col justify-center items-center">
       <Image
         src="/logo.webp"
-        alt="Logo con el título Examen de Ingreso-inador"
+        alt="Examen de Ingreso-inador: Encuentra exámenes pasados para ingresar a la Facultad de Tecnología de la UMSS"
         className="rounded-3xl animate-fadeDown"
         width={384}
         height={284}
