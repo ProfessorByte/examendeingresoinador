b6f03351f3b6d58aa20e2993520f9253c97e4512
Pablo Pardo <pablopardoalcocer@gmail.com>
2025-10-01 18:25:32 -0400

    Fix URL in getFcytExamsData function to point to the correct repository for valid URLs

--- src/utils/services.ts
+++ src/utils/services.ts
@@ -2,7 +2,7 @@ import type { Exam } from "../types/exam.types";
 
 export const getFcytExamsData = async (): Promise<Exam[]> => {
   const response = await fetch(
-    "https://raw.githubusercontent.com/ProfessorByte/FCyT_UMSS_ExamsPseudocrawler/refs/heads/main/validUrls.json"
+    "https://raw.githubusercontent.com/ProfessorByte/science-exams-crawler/refs/heads/main/validUrls.json"
   );
   const data = await response.json();
   return data;
