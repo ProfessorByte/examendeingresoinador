8101b4aa99c7b880df7397b8f50fb968c295f92b
Pablo Pardo <pablopardoalcocer@gmail.com>
2025-12-07 18:35:52 -0400

    Update FCYT_EXAMS_CRAWLER_BASE_URL to point to the main branch

--- src/utils/baseUrls.ts
+++ src/utils/baseUrls.ts
@@ -1,2 +1,2 @@
 export const FCYT_EXAMS_CRAWLER_BASE_URL =
-  "https://raw.githubusercontent.com/ProfessorByte/science-exams-crawler/refs/heads/develop";
+  "https://raw.githubusercontent.com/ProfessorByte/science-exams-crawler/refs/heads/main";
