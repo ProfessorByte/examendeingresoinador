aec597a7ecd83050cc05da29ee9302ec8fc778b5
Pablo Pardo <pablopardoalcocer@gmail.com>
2025-12-10 06:44:52 -0400

    Configure for Next.js 15

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+  experimental: {
+    reactCompiler: true,
+  },
   redirects: async () => {
     return [
       {
